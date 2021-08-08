91978df53cd4ccf233424d85115c322b27ddef73
alex <alexlhliang@gmail.com>
2021-08-07 23:09:38 -0700

    fixed today bug

--- src/today.js
+++ src/today.js
@@ -6,16 +6,16 @@ const today = (() => {
       const today = new Date();
       const year = today.getFullYear();
       let month = today.getMonth() + 1;
-      if (month.toString().length === 1) month = "0".concat(month.toString());
-      const day = today.getDate();
-      if (day.toString().length === 1) day = "0".concat(day.toString());
+      if (month.toString().length === 1) month = '0'.concat(month.toString());
+      let day = today.getDate();
+      if (day.toString().length === 1) day = '0'.concat(day.toString());
       const todayStr = `${year}-${month}-${day}`;
       return todayStr;
     },
     getTodayTodos(list) {
       const today = this.getToday();
       const todoList = list.filter((item) => {
-        if (item.priority === "5") return false;
+        if (item.priority === '5') return false;
         return item.day == today;
       });
       return todoList;
@@ -24,8 +24,8 @@ const today = (() => {
       const today = new Date();
       today.setHours(0, 0, 0, 0);
       const todoList = list.filter((item) => {
-        if (item.priority === "5") return false;
-        const dueDay = new Date(item.day + " 00:00");
+        if (item.priority === '5') return false;
+        const dueDay = new Date(item.day + ' 00:00');
         return dueDay < today;
       });
       return todoList;
